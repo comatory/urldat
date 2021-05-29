0d07fcf6bc4a539333eed89f6ed4e30549837ecc
Ondřej Synáček <ondrejsynacek@fastmail.com>
2021-05-29 15:40:23 +0200

    fix: text in specs

--- test/urlcat_test.dart
+++ test/urlcat_test.dart
@@ -4,43 +4,45 @@ import 'package:urlcat/urlcat.dart';
 
 void main() {
   group('urlcat', () {
-    test('Concatenates the base URL and the path if no params are passed', () {
+    test('should concatenate the base URL and the path if no params are passed',
+        () {
       expect(urlcat('http://example.com', 'path'),
           equals('http://example.com/path'));
     });
 
     test(
-        'Uses exactly one slash for joining even if the base URL has a trailing slash',
+        'should add exactly one slash for joining even if the base URL has a trailing slash',
         () {
       expect(urlcat('http://example.com/', 'path'),
           equals('http://example.com/path'));
     });
 
     test(
-        'Uses exactly one slash for joining even if the path has a leading slash',
+        'should add exactly one slash for joining even if the path has a leading slash',
         () {
       expect(urlcat('http://example.com', '/path'),
           equals('http://example.com/path'));
     });
 
     test(
-        'Uses exactly one slash for joining even if the base URL and the path both have a slash at the boundary',
+        'should add exactly one slash for joining even if the base URL and the path both have a slash at the boundary',
         () {
       expect(urlcat('http://example.com/', '/path'),
           equals('http://example.com/path'));
     });
 
-    test('Substitutes path parameters', () {
+    test('should substitute path parameters', () {
       expect(urlcat('http://example.com/', '/path/:p', parameters: {'p': 1}),
           'http://example.com/path/1');
     });
 
-    test('Allows path parameters at the beginning of the path', () {
+    test('should allow path parameters at the beginning of the path', () {
       expect(urlcat('http://example.com/', ':p', parameters: {'p': 1}),
           equals('http://example.com/1'));
     });
 
-    test('Parameters that are missing from the path become query parameters',
+    test(
+        'should convert parameters that are missing from the path to query parameters',
         () {
       expect(
           urlcat('http://example.com/', '/path/:p',
@@ -49,27 +51,29 @@ void main() {
     });
 
     test(
-        'Uses exactly one ? to join query parameters even if the path has a trailing question mark',
+        'should use exactly one ? to join query parameters even if the path has a trailing question mark',
         () {
       expect(urlcat('http://example.com/', '/path?', parameters: {'q': 2}),
           equals('http://example.com/path?q=2'));
     });
 
     test(
-        'Removes trailing question mark from the path if no params are specified',
+        'should removes trailing question mark from the path if no params are specified',
         () {
       expect(urlcat('http://example.com/', '/path?', parameters: {}),
           equals('http://example.com/path'));
     });
 
-    test('All parameters become query parameters if the path has no parameters',
+    test(
+        'should convert all parameters to query parameters if the path has no parameters',
         () {
       expect(
           urlcat('http://example.com/', '/path?', parameters: {'p': 1, 'q': 2}),
           equals('http://example.com/path?p=1&q=2'));
     });
 
-    test('If a parameter appears twice in the path, it is substituted twice',
+    test(
+        'should substitute parameter twice If a parameter appears twice in the path',
         () {
       expect(
           urlcat('http://example.com/', '/path/:p1/:p2/:p1/r',
@@ -77,14 +81,14 @@ void main() {
           equals('http://example.com/path/a/b/a/r'));
     });
 
-    test('Escapes both path and query parameters', () {
+    test('should escapes both path and query parameters', () {
       expect(
           urlcat('http://example.com/', '/path/:p',
               parameters: {'p': 'a b', 'q': 'b c'}),
           equals('http://example.com/path/a%20b?q=b+c'));
     });
 
-    test('Can handle complex URLs', () {
+    test('should handle complex URLs', () {
       expect(
           urlcat('http://example.com/', '/users/:userId/posts/:postId/comments',
               parameters: {
@@ -98,7 +102,8 @@ void main() {
               'http://example.com/users/123/posts/987/comments?authorId=456&limit=10&offset=120'));
     });
 
-    test('Provides an overload (baseUrl, pathTemplate) that works correctly',
+    test(
+        'should Provide an overload (baseUrl, pathTemplate) that works correctly',
         () {
       expect(urlcat('http://example.com/', '/path'),
           equals('http://example.com/path'));
@@ -108,33 +113,33 @@ void main() {
              but in Dart it's not needed:
              https://github.com/balazsbotond/urlcat/issues/7
     */
-    test('Handles "//" path correctly', () {
+    test('should handles "//" path correctly', () {
       expect(
           urlcat('http://example.com/', '//'), equals('http://example.com/'));
     });
 
-    test('Escape empty path params', () {
+    test('should escape empty path params', () {
       expect(urlcat('http://example.com/path', '', parameters: {'p': 'a'}),
           equals('http://example.com/path?p=a'));
     });
 
-    test('Renders boolean (true) path params', () {
+    test('should encode boolean (true) path params', () {
       expect(urlcat('http://example.com', '/path/:p', parameters: {'p': true}),
           equals('http://example.com/path/true'));
     });
 
-    test('Renders number path params', () {
+    test('should encode number path params', () {
       expect(urlcat('http://example.com/', 'path/:p', parameters: {'p': 456}),
           equals('http://example.com/path/456'));
     });
 
-    test('Renders string path params', () {
+    test('should encode string path params', () {
       expect(
           urlcat('http://example.com', '/path/:p', parameters: {'p': 'test'}),
           equals('http://example.com/path/test'));
     });
 
-    test('Ignores entirely numeric path params', () {
+    test('should ignores numeric path params entirely', () {
       expect(
           urlcat('http://localhost:3000', '/path/:p',
               parameters: {'p': 'test'}),
